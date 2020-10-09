bf52515854efa50d9b11cc4e4d15336793e1b44b
sliu2200899 <sliu2200899@gmail.com>
2020-10-09 11:06:30 -0700

    swap pair recursive

--- src/com/jetbrains/master/SwapPairs.java
+++ src/com/jetbrains/master/SwapPairs.java
@@ -25,4 +25,33 @@ public class SwapPairs {
 
         return dummy.next;
     }
-}
+
+    // recursive methods
+    /*
+    1 Start the recursion with head node of the original linked list.
+
+    2 Every recursion call is responsible for swapping a pair of nodes. Let's represent the two nodes to be swapped by firstNode and secondNode.
+
+    3 Next recursion is made by calling the function with head of the next pair of nodes. This call would swap the next two nodes and make further recursive calls
+    if there are nodes left in the linked list.
+
+    4 Once we get the pointer to the remaining swapped list from the recursion call, we can swap the firstNode and secondNode i.e. the nodes in the current recursive
+     call and then return the pointer to the secondNode since it will be the new head after swapping.
+     */
+    public ListNode swapPairs2(ListNode head) {
+        // base case
+        // if the list has no node or has only one node left
+        if (head == null || head.next == null) return head;
+
+        // recursive case
+        // Nodes to be swapped
+        ListNode firstNode = head;
+        ListNode secondNode = head.next;
+
+        // Swapping
+        firstNode.next = swapPairs(secondNode.next);
+        secondNode.next = firstNode;
+
+        // Now head is the second node
+        return secondNode;
+    }
