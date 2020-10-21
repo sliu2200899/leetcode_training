27a5d13f006ebdf4a64d52f1e76fba539ecc5707
sliu2200899 <sliu2200899@gmail.com>
2020-10-21 13:00:50 -0700

    recursive 3sum

--- src/com/jetbrains/classic/ThreeSum.java
+++ src/com/jetbrains/classic/ThreeSum.java
@@ -1,8 +1,6 @@
 package com.jetbrains.classic;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 public class ThreeSum {
     public List<List<Integer>> threeSum(int[] nums) {
@@ -43,4 +41,38 @@ public class ThreeSum {
     }
 
 
+    /*
+        follow up: 4sum, 5sum, 6sum
+        how to come up with a generalized approach to solve this problem
+     */
+
+    public List<List<Integer>> fourSum(int[] nums, int target) {
+        Arrays.sort(nums);
+        return kSum(nums, target, 0, 4);
+    }
+    public List<List<Integer>> kSum(int[] nums, int target, int start, int k) {
+        List<List<Integer>> res = new ArrayList<>();
+        if (start == nums.length || nums[start] * k > target || target > nums[nums.length - 1] * k)
+            return res;
+        if (k == 2)
+            return twoSum(nums, target, start);
+        for (int i = start; i < nums.length; ++i)
+            if (i == start || nums[i - 1] != nums[i]) {}
+//                for (var set : kSum(nums, target - nums[i], i + 1, k - 1)) {    //java 10 var
+//                    res.add(new ArrayList<>(Arrays.asList(nums[i])));
+//                    res.get(res.size() - 1).addAll(set);
+//                }
+        return res;
+    }
+    public List<List<Integer>> twoSum(int[] nums, int target, int start) {
+        List<List<Integer>> res = new ArrayList<>();
+        Set<Integer> s = new HashSet<>();
+        for (int i = start; i < nums.length; ++i) {
+            if (res.isEmpty() || res.get(res.size() - 1).get(1) != nums[i])
+                if (s.contains(target - nums[i]))
+                    res.add(Arrays.asList(target - nums[i], nums[i]));
+            s.add(nums[i]);
+        }
+        return res;
+    }
 }
