fdc5d2decf6680c15d136acc458df63f044c9e8d
sliu2200899 <sliu2200899@gmail.com>
2020-10-26 23:24:20 -0700

    sliding window

--- src/com/jetbrains/classic/LengthOfLongestSubstring.java
+++ src/com/jetbrains/classic/LengthOfLongestSubstring.java
@@ -1,10 +1,52 @@
 package com.jetbrains.classic;
 
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 
 public class LengthOfLongestSubstring {
+
+    /*
+        first Sliding window method, 2 times visit
+        In the naive approaches, we repeatedly check a substring to see if it has duplicate character.
+        But it is unnecessary.
+
+        By using HashSet as a sliding window, checking if a character in the current can be done in O(1)
+        A sliding window is an abstract concept commonly used in array/string problems.
+        A window is a range of elements in the array/string which usually defined by the start and end indices, i.e. [i, j) (left-closed, right-open).
+        A sliding window is a window "slides" its two boundaries to the certain direction. For example, if we slide [i, j) to the right by 11 element, then it becomes [i+1, j+1)(left-closed, right-open).
+
+
+        time complexity: O(2n)
+        space complexity: O(min(m,n)
+     */
     public int lengthOfLongestSubstring(String s) {
+        int n = s.length();
+        Set<Character> set = new HashSet<>();
+        int ans = 0, i = 0, j = 0;
+        while (i < n && j < n) {
+            // try to extend the range [i, j]
+            if (!set.contains(s.charAt(j))){
+                set.add(s.charAt(j++));
+                ans = Math.max(ans, j - i);
+            }
+            else {
+                set.remove(s.charAt(i++));
+            }
+        }
+        return ans;
+    }
+
+
+    /*
+        Time complexity : O(n). Index j will iterate n times.
+        Space complexity : O(min(m, n)). Same as the previous approach.
+            We need O(k) space for the sliding window, where k is the size of the Set.
+            The size of the Set is upper bounded by the size of the string n and the size
+            of the charset/alphabet mm.
+     */
+    public int lengthOfLongestSubstring2(String s) {
         if (s == null || s.length() == 0) return 0;
 
         Map<Character, Integer> map = new HashMap<>();
--- src/com/jetbrains/master/LengthOfLongestSubstringKDistinct.java
+++ src/com/jetbrains/master/LengthOfLongestSubstringKDistinct.java
@@ -1,6 +1,7 @@
 package com.jetbrains.master;
 
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
 
 public class LengthOfLongestSubstringKDistinct {
@@ -25,5 +26,45 @@ public class LengthOfLongestSubstringKDistinct {
         }
 
         return len;
+    }
+
+    /*
+        time complexity: O(n) and single pass
+     */
+    public int lengthOfLongestSubstringKDistinct2(String s, int k) {
+        int n = s.length();
+        if (n*k == 0) return 0;
+
+        // sliding window left and right pointers
+        int left = 0;
+        int right = 0;
+        // hashmap character -> its rightmost position
+        // in the sliding window
+        LinkedHashMap<Character, Integer> hashmap = new LinkedHashMap<Character, Integer>(k + 1);
+
+        int max_len = 1;
+
+        while (right < n) {
+            Character character = s.charAt(right);
+            // if character is already in the hashmap -
+            // delete it, so that after insert it becomes
+            // the rightmost element in the hashmap
+            if (hashmap.containsKey(character))
+                hashmap.remove(character);
+            hashmap.put(character, right++);
+
+            // slidewindow contains k + 1 characters
+            if (hashmap.size() == k + 1) {
+                // delete the leftmost character
+                Map.Entry<Character, Integer> leftmost = hashmap.entrySet().iterator().next();
+                System.out.println(leftmost.getKey());
+                hashmap.remove(leftmost.getKey());
+                // move left pointer of the slidewindow
+                left = leftmost.getValue() + 1;
+            }
+
+            max_len = Math.max(max_len, right - left);
+        }
+        return max_len;
     }
 }
