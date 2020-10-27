c36ce241b50ef5ec6e587633416a528e9430955c
sliu2200899 <sliu2200899@gmail.com>
2020-10-27 12:54:13 -0700

    dp

--- src/com/jetbrains/hard/UniqueBST.java
+++ src/com/jetbrains/hard/UniqueBST.java
@@ -2,10 +2,7 @@ package com.jetbrains.hard;
 
 import com.jetbrains.innerStructure.TreeNode;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class UniqueBST {
     Map<Integer, Integer> map;
@@ -37,6 +34,55 @@ public class UniqueBST {
 
         return total;
     }
+
+
+    /*
+        another kind of memorization with resursion
+        The problem can be solved in a dynamic programming way.
+
+Given a sorted sequence 1 ... n, to construct a Binary Search Tree (BST) out of the sequence, we could enumerate each number i in the sequence,
+and use the number as the root, then, the subsequence 1 ... (i-1) on its left side would lay on the left branch of the root, and similarly the right subsequence (i+1) ... n lay on the right branch of the root.
+We then can construct the subtree from the subsequence recursively. Through the above approach, we could be assured that the BST that we construct are all unique, since they start from unique roots.
+
+As we can see, the problem can be reduced into problems with smaller sizes, instead of recursively (also repeatedly) solve the subproblems, we can store the solution of subproblems and reuse them later, i.e. the dynamic programming way.
+
+Time complexity : the main computation of the algorithm is done at the statement with G[i]. So the time complexity is essentially the number of iterations for the statement,therefore the time complexity is O(N^2)
+Space complexity : The space complexity of the above algorithm is mainly the storage to keep all the intermediate solutions, therefore O(N).
+     */
+    public int numTrees2(int n) {
+        if (n == 0 || n == 1) return 1;
+
+        int[] arr = new int[n + 1];
+        Arrays.fill(arr, -1);
+
+        return helper(n, arr);
+    }
+
+    private int helper(int n, int[] arr) {
+
+        if (arr[n] == -1) {
+
+            if (n == 0 || n == 1) {
+                arr[n] = 1;
+                return arr[n];
+            }
+
+            if (n == 2) {
+                arr[n] = 2;
+                return arr[n];
+            }
+
+            int num = 0;
+            for (int i = 1; i <= n; ++i) {
+                num += (helper(i - 1, arr) * helper(n - i, arr));
+            }
+            arr[n] = num;
+        }
+
+        return arr[n];
+    }
+
+
 
     
     // follow up, print all the tree node
