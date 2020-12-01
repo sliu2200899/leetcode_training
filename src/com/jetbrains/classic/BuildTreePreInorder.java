8825b23ad0840398c8cc469dc15096696d78bae0
sliu2200899 <sliu2200899@gmail.com>
2020-11-30 22:34:23 -0800

    construct bt using pre and in

--- src/com/jetbrains/classic/BuildTreePreInorder.java
+++ src/com/jetbrains/classic/BuildTreePreInorder.java
@@ -4,35 +4,31 @@ import com.jetbrains.innerStructure.TreeNode;
 
 public class BuildTreePreInorder {
     public TreeNode buildTree(int[] preorder, int[] inorder) {
-        if (preorder == null || inorder == null ||
-                preorder.length == 0 || inorder.length == 0) {
-            return null;
-        }
+        if (preorder == null || inorder == null) return null;
 
-        return build(preorder, 0, inorder, 0, preorder.length);
+        return helper(preorder, 0, inorder, 0, preorder.length);
     }
 
-    // s1: start index of preorder array
-    // s2: start index of inorder array
-    // len: the length of the current preorder subarray
-    private TreeNode build(int[] pre, int s1, int[] in, int s2, int len) {
+    private TreeNode helper(int[] pre, int preStart, int[] in, int inStart, int len) {
         // base case
         if (len <= 0) return null;
-        if (len == 1) return new TreeNode(pre[s1]);
+        if (len == 1) return new TreeNode(pre[preStart]);
 
         // recursive case
-        int rootVal = pre[s1]; // root value
-        int pos = s2;   // find the position of the root value in the inorder array
-        while (pos < in.length && in[pos] != rootVal) {
-            pos++;
+        int rootVal = pre[preStart];
+
+        // find the index in the array in
+        int i = 0;
+        while (i < len && in[i + inStart] != rootVal) {
+            i++;
         }
-        //we can split both inorder and preorder arrays into three parts.
-        int leftLen = pos - s2;   // the length of partA subarray
-        int rightLen = len - 1 - leftLen; // the length of partB subarray
+
+        int leftLen = i;
+        int rightLen = len - i - 1;
 
         TreeNode root = new TreeNode(rootVal);
-        root.left = build(pre, s1 + 1, in, s2, leftLen);
-        root.right = build(pre, s1 + leftLen + 1, in, pos + 1, rightLen);
+        root.left = helper(pre, preStart + 1, in, inStart, leftLen);
+        root.right = helper(pre, preStart + 1 + leftLen, in, inStart + 1 + leftLen, rightLen);
 
         return root;
     }
