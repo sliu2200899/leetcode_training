47c5217a6dae31586a71b7a6fcb046f7764d883c
sliu2200899 <sliu2200899@gmail.com>
2021-01-15 11:46:05 -0800

    maximal square

--- src/com/jetbrains/classic/ideologyAlgo/dynamicProgramming/MaximalSquare.java
+++ src/com/jetbrains/classic/ideologyAlgo/dynamicProgramming/MaximalSquare.java
@@ -65,6 +65,8 @@ public class MaximalSquare {
         We also remember the size of the largest square found so far. In this way, we traverse the original matrix once and find out the required maximum size.
         This gives the side length of the square (say maxsqlenmaxsqlen). The required result is the area maxsqlen^2
  .
+        time: O(mn)
+        space: O(mn)
      */
     public int maximalSquare2(char[][] matrix) {
         int rows = matrix.length, cols = rows > 0 ? matrix[0].length : 0;
@@ -76,6 +78,33 @@ public class MaximalSquare {
                     dp[i][j] = Math.min(Math.min(dp[i][j - 1], dp[i - 1][j]), dp[i - 1][j - 1]) + 1;
                     maxsqlen = Math.max(maxsqlen, dp[i][j]);
                 }
+            }
+        }
+        return maxsqlen * maxsqlen;
+    }
+
+    /*
+        approach 3: better dynamic programming
+        improve the space complexity further more...
+
+        time: O(mn)
+        space: O(n)
+     */
+
+    public int maximalSquare3(char[][] matrix) {
+        int rows = matrix.length, cols = rows > 0 ? matrix[0].length : 0;
+        int[] dp = new int[cols + 1];
+        int maxsqlen = 0, prev = 0;
+        for (int i = 1; i <= rows; i++) {
+            for (int j = 1; j <= cols; j++) {
+                int temp = dp[j];
+                if (matrix[i - 1][j - 1] == '1') {
+                    dp[j] = Math.min(Math.min(dp[j - 1], prev), dp[j]) + 1;
+                    maxsqlen = Math.max(maxsqlen, dp[j]);
+                } else {
+                    dp[j] = 0;
+                }
+                prev = temp;
             }
         }
         return maxsqlen * maxsqlen;
