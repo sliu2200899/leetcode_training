96babfc006f4f2f2c6943af2996f50c3b096c2cb
sliu2200899 <sliu2200899@gmail.com>
2021-01-18 19:37:25 -0800

    word break 2

--- src/com/jetbrains/classic/ideologyAlgo/dynamicProgramming/WordBreak2.java
+++ src/com/jetbrains/classic/ideologyAlgo/dynamicProgramming/WordBreak2.java
@@ -1,5 +1,34 @@
 package com.jetbrains.classic.ideologyAlgo.dynamicProgramming;
 
-public class WordBreak2 {
+import java.util.*;
 
+public class WordBreak2 {
+    public List<String> wordBreak(String s, List<String> wordDict) {
+        Map<String, List<String>> map = new HashMap<>();
+        Set<String> set = new HashSet<>(wordDict);
+        return helper(s, map, set);
+    }
+
+    private List<String> helper(String s, Map<String, List<String>>map,  Set<String> set) {
+        // in memory, directly return
+        if (map.containsKey(s)) return map.get(s);
+
+        List<String> res = new ArrayList<>();
+        if (s.length() == 0) {
+            res.add("");
+            return res;
+        }
+
+        for (String word: set) {
+            if (s.endsWith(word)) {
+                List<String> sublist = helper(s.substring(0, (s.length() - word.length())), map, set);
+                for (String sub : sublist) {
+                    res.add(sub + (sub.isEmpty() ? "" : " ") + word);
+                }
+            }
+        }
+
+        map.put(s, res);
+        return res;
+    }
 }
