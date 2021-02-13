e6b596016f524e67e6401811a89b95ce2ecc5aed
sliu2200899 <sliu2200899@gmail.com>
2021-02-13 10:50:40 -0800

    n queens

--- src/com/jetbrains/classic/searchByAlgorithm/dfs/NQueens.java
+++ src/com/jetbrains/classic/searchByAlgorithm/dfs/NQueens.java
@@ -1,5 +1,135 @@
 package com.jetbrains.classic.searchByAlgorithm.dfs;
 
-public class NQueens {
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
+public class NQueens {
+    /*
+
+
+        algo:
+            available(x, y):    x is col y is row
+                return !col[x]
+                     and !diag1(x,y)    // diag1(x,y)   idx = x + y    from top left to bottom right
+                     and !diag2(x,y)   // diag2(x,y)    idx = x - y + (n - 1)   from bottom left to top right
+
+            n_queen(y, n, b, ans):
+                if y == n:
+                    ans += b
+                    return
+
+                for x in range(0, n):
+                    if not available(x,y): continue
+                    put(x, y, b)
+                    n_queen(y+1, n, b, ans)
+                    remove(x, y, b)
+     */
+
+    public List<List<String>> solveNQueens(int n) {
+        boolean[] cols = new boolean[n];
+        boolean[] diag1 = new boolean[2*n-1];
+        boolean[] diag2 = new boolean[2*n-1];
+
+        List<List<String>> res = new ArrayList<>();
+
+        // init
+        char[][] chess = new char[n][n];
+        for (char[] tmp: chess) Arrays.fill(tmp, '.');
+
+        // dfs
+        dfs(chess, res, 0, n, cols, diag1, diag2);
+        return res;
+    }
+
+
+    // how to add result to the returning array
+    // how to prunning
+    private void dfs(char[][] chess, List<List<String>> res, int depth, int n,
+                     boolean[] cols, boolean[] diag1, boolean[] diag2) {
+        // base case
+        if (depth == n) {
+            addSolution(res, chess);
+            return;
+        }
+
+        // recursive case
+        for (int j = 0; j < n; ++j) {
+            if (!cols[j] && !diag1[depth + j] && !diag2[j - depth + (n - 1)]) {
+
+                cols[j] = true;
+                diag1[depth + j] = true;
+                diag2[j - depth + (n - 1)] = true;
+                chess[depth][j] = 'Q';
+
+                dfs(chess, res, depth+1, n, cols, diag1, diag2);
+
+                chess[depth][j] = '.';
+                diag2[j - depth + (n - 1)] = false;
+                diag1[depth + j] = false;
+                cols[j] = false;
+            }
+        }
+    }
+
+    private void addSolution(List<List<String>> res, char[][] chess) {
+        List<String> list = new ArrayList<>();
+        for (char[] row : chess) {
+            list.add(String.valueOf(row));
+        }
+
+        res.add(list);
+    }
+
+
+    /*
+        n queens 2:
+        similar to the n_queen 1, and cannot use memoization to simplify the problem
+        the difference is that
+            1 do not need to add result to the returning array
+            2 how to return the total number instead of using ugly global variable?
+     */
+    public int totalNQueens(int n) {
+        boolean[] cols = new boolean[n];
+        boolean[] diag1 = new boolean[2*n-1];
+        boolean[] diag2 = new boolean[2*n-1];
+
+        List<List<String>> res = new ArrayList<>();
+
+        // init
+        char[][] chess = new char[n][n];
+        for (char[] tmp: chess) Arrays.fill(tmp, '.');
+
+        // dfs
+        return dfs2(chess, res, 0, n, cols, diag1, diag2);
+    }
+
+    private int dfs2(char[][] chess, List<List<String>> res, int depth, int n,
+                    boolean[] cols, boolean[] diag1, boolean[] diag2) {
+        // base case
+        if (depth == n) {
+            return 1;
+        }
+
+        // recursive case
+        int num = 0;
+        for (int j = 0; j < n; ++j) {
+            if (!cols[j] && !diag1[depth + j] && !diag2[j - depth + (n - 1)]) {
+
+                cols[j] = true;
+                diag1[depth + j] = true;
+                diag2[j - depth + (n - 1)] = true;
+                chess[depth][j] = 'Q';
+
+                num += dfs2(chess, res, depth+1, n, cols, diag1, diag2);
+
+                chess[depth][j] = '.';
+                diag2[j - depth + (n - 1)] = false;
+                diag1[depth + j] = false;
+                cols[j] = false;
+            }
+        }
+
+        return num;
+    }
 }
---
+++ src/com/jetbrains/classic/searchByAlgorithm/dfs/PalindromePartition.java
@@ -0,0 +1,87 @@
+package com.jetbrains.classic.searchByAlgorithm.dfs;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PalindromePartition {
+
+    /*
+        time: O(N * 2^N)    recursion tree  in the worst case
+        space: O(N) the recursion stack, equivalent to the length of s
+
+        Algorithm
+
+        In the backtracking algorithm, we recursively traverse over the string in depth-first search fashion. For each recursive call, the beginning index of the string is given as \text{start}start.
+
+            Iteratively generate all possible substrings beginning at \text{start}start index. The \text{end}end index increments from \text{start}start till the end of the string.
+
+            For each of the substring generated, check if it is a palindrome.
+
+            If the substring is a palindrome, the substring is a potential candidate. Add substring to the \text{currentList}currentList and perform a depth-first search on the remaining substring.
+            If current substring ends at index \text{end}end, \text{end}+1end+1 becomes the \text{start}start index for the next recursive call.
+
+            Backtrack if \text{start}start index is greater than or equal to the string length and add the \text{currentList}currentList to the result.
+     */
+    public List<List<String>> partition(String s) {
+        List<List<String>> result = new ArrayList<>();
+
+        dfs(0, result, new ArrayList<String>(), s);
+
+        return result;
+    }
+
+    private void dfs(int start, List<List<String>> result, List<String> currentList, String s) {
+        if (start >= s.length()) {
+            result.add(new ArrayList<String>(currentList));
+        }
+
+        for (int end = start; end < s.length(); end++) {
+            if(isPalindrome(s, start, end)) {
+                // add current substring in the currentList
+                currentList.add(s.substring(start, end + 1));
+
+                dfs(end + 1, result, currentList, s);
+
+                // backtrack and remove the current substring from currentList
+                currentList.remove(currentList.size() - 1);
+            }
+        }
+    }
+
+    private boolean isPalindrome(String s, int low, int high) {
+        while(low < high) {
+            if (s.charAt(low++) != s.charAt(high--)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+
+    /*
+        backtracking with DP
+     */
+    public List<List<String>> partition2(String s) {
+        int len = s.length();
+        boolean[][] dp = new boolean[len][len];
+        List<List<String>> result = new ArrayList<>();
+        dfs(result, s, 0, new ArrayList<>(), dp);
+        return result;
+    }
+
+    void dfs(List<List<String>> result, String s, int start, List<String> currentList, boolean[][] dp) {
+        if (start >= s.length()) {
+            result.add(new ArrayList<>(currentList));
+        }
+
+        for (int end = start; end < s.length(); ++end) {
+            if (s.charAt(start) == s.charAt(end) && (end - start <= 2 || dp[start+1][end-1])) {
+                dp[start][end] = true;
+                currentList.add(s.substring(start, end + 1));
+                dfs(result, s, end + 1, currentList, dp);
+                currentList.remove(currentList.size() - 1);
+            }
+        }
+    }
+}
