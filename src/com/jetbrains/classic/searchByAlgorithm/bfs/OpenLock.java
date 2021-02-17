55caa797214a4e4def2268a74da2f03fa8d6322c
sliu2200899 <sliu2200899@gmail.com>
2021-02-17 12:27:38 -0800

    word ladder bi-direction bfs

--- src/com/jetbrains/classic/searchByAlgorithm/bfs/OpenLock.java
+++ src/com/jetbrains/classic/searchByAlgorithm/bfs/OpenLock.java
@@ -54,4 +54,60 @@ public class OpenLock {
         }
         return nt;
     }
+
+    /*
+        bi-directional BFS
+
+        key point is that use dead set to serve as visited set. So each time we process one element, we jsut put it in
+                the dead set
+
+        refer to:  https://www.geeksforgeeks.org/bidirectional-search/
+
+        time: O(m^2 * n)
+        space: O(m^2 * n)
+     */
+    public int openLock2(String[] deadends, String target) {
+        Set<String> beginSet = new HashSet<>();
+        Set<String> endSet = new HashSet<>();
+
+        Set<String> deads = new HashSet<>(Arrays.asList(deadends));
+        beginSet.add("0000");
+        endSet.add(target);
+
+        int level = 0;
+        while (!beginSet.isEmpty() && !endSet.isEmpty()) {
+            if (beginSet.size() > endSet.size()) {
+                Set<String> temp = endSet;
+                endSet = beginSet;
+                beginSet = temp;
+            }
+
+            Set<String> tmp = new HashSet<>();
+            for (String cur : beginSet) {
+
+                if (endSet.contains(cur)) return level;
+                if (deads.contains(cur)) continue;
+                deads.add(cur);
+
+                char[] arr = cur.toCharArray();
+                for (int i = 0; i < arr.length; ++i) {
+                    char c = arr[i];
+                    String s1 = cur.substring(0, i) + (c == '9' ? 0 : c - '0' + 1) + cur.substring(i + 1);
+                    String s2 = cur.substring(0, i) + (c == '0' ? 9 : c - '0' - 1) + cur.substring(i + 1);
+
+                    if (!deads.contains(s1)) {
+                        tmp.add(s1);
+                    }
+
+                    if (!deads.contains(s2)) {
+                        tmp.add(s2);
+                    }
+                }
+            }
+            level++;
+            beginSet = tmp;
+        }
+
+        return -1;
+    }
 }
--- src/com/jetbrains/classic/searchByAlgorithm/bfs/WordLadder.java
+++ src/com/jetbrains/classic/searchByAlgorithm/bfs/WordLadder.java
@@ -128,4 +128,74 @@ public class WordLadder {
         }
         return res;
     }
+
+    /*
+        solution 3: bi-directional bfs
+
+        algo:
+            The algorithm is very similar to the standard BFS based approach we saw earlier.
+
+            The only difference is we now do BFS starting two nodes instead of one. This also changes the termination condition of our search.
+
+            We now have two visited dictionaries to keep track of nodes visited from the search starting at the respective ends.
+
+            If we ever find a node/word which is in the visited dictionary of the parallel search we terminate our search, since we have found the meet point of this bidirectional search.
+            It's more like meeting in the middle instead of going all the way through.
+
+            Termination condition for bidirectional search is finding a word which is already been seen by the parallel search.
+
+            The shortest transformation sequence is the sum of levels of the meet point node from both the ends.
+            Thus, for every visited node we save its level as value in the visited dictionary.
+
+        time: O(M^2 * N) where M is the length of words and N is the total number of words in the input word list. Similar to one directional, bidirectional also takes
+                O(M^2 * N) time for finding out all the transformations. But the search time reduces to half, since the two parallel searches meet somewhere in the middle.
+        space:  O(M^2 * N) to store all M transformations for each of the N words in the all_combo_dict dictionary, same as one directional.
+                But bidirectional reduces the search space. It narrows down because of meeting in the middle.
+     */
+
+    public int ladderLength3(String beginWord, String endWord, List<String> wordList) {
+        if (!wordList.contains(endWord)) return 0;
+
+        Set<String> beginSet = new HashSet<>();
+        Set<String> endSet = new HashSet<>();
+
+        Set<String> visited = new HashSet<>(wordList);
+        beginSet.add(beginWord);
+        endSet.add(endWord);
+
+        visited.add(beginWord);
+
+        int step = 1;
+        while (!beginSet.isEmpty() && !endSet.isEmpty()) {
+            step++;
+            if (beginSet.size() > endSet.size()) {
+                Set<String> tmp = endSet;
+                endSet = beginSet;
+                beginSet = tmp;
+            }
+
+            Set<String> temp = new HashSet<>();
+            for (String cur : beginSet) {
+                for (int i = 0; i < cur.length(); ++i) {
+                    char[] arr = cur.toCharArray();
+                    for (char c = 'a'; c <= 'z'; ++c) {
+                        arr[i] = c;
+                        String newStr = String.valueOf(arr);
+                        if (cur.equals(newStr)) continue;
+                        if (endSet.contains(newStr)) {
+                            return step;
+                        }
+
+                        if (visited.contains(newStr)) {
+                            temp.add(newStr);
+                            visited.remove(newStr);
+                        }
+                    }
+                }
+            }
+            beginSet = temp;
+        }
+
+        return 0;
+    }
 }
