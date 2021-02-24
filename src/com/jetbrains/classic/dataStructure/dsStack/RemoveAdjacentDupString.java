a71bd5b26861d5018bf66d2858ae224661a8c071
sliu2200899 <sliu2200899@gmail.com>
2021-02-24 12:23:55 -0800

    stack

---
+++ src/com/jetbrains/classic/dataStructure/dsStack/RemoveAdjacentDupString.java
@@ -0,0 +1,25 @@
+package com.jetbrains.classic.dataStructure.dsStack;
+
+public class RemoveAdjacentDupString {
+
+    /*
+        Solving the problem using Stack
+        Stack is suitable for the situation like need to delay processing elements or do some computation in the middle
+        another thing need to notice is that we can use StringBuilder to act as Stack
+     */
+    public String removeDuplicates(String S) {
+        StringBuilder sb = new StringBuilder();
+        int len = 0;
+        for (char c : S.toCharArray()) {
+            if (len != 0 && c == sb.charAt(len - 1)) {
+                sb.deleteCharAt(len - 1);
+                len--;
+            }
+            else {
+                sb.append(c);
+                len++;
+            }
+        }
+        return sb.toString();
+    }
+}
