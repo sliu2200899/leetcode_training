c0207238222f3ed37792989354c137a768c59a52
sliu2200899 <sliu2200899@gmail.com>
2021-03-08 23:46:25 -0800

    dfs note

--- src/com/jetbrains/classic/searchByAlgorithm/dfs/Permutation.java
+++ src/com/jetbrains/classic/searchByAlgorithm/dfs/Permutation.java
@@ -3,6 +3,25 @@ package com.jetbrains.classic.searchByAlgorithm.dfs;
 import java.util.*;
 
 public class Permutation {
+    /*
+    Analyzing the Time Complexity :
+
+        1. How many times does function perm get called in its base case?
+        As we can understand from the recursion explained above that for a string of length 3 it is printing 6 permutations which is actually 3!.
+        This is because if it needs to generate permutation, it is needed to pick characters for each slot.
+        If there are 3 characters in our string, in the first slot, there are 3 choices, 2 choices for the next slot (for each of 3 choices earlier, i.e multiplication and not addition) and so on.
+        This tells that there are n! permutations being printed in the base case which is what is shown in the image.
+
+        2. How many times does function perm get called before its base case?
+        Consider that lines 9 through 12 are hit n number of times. Therefore, there will be no more than (n * n!) function calls.
+
+        3. How long does each function call take?
+        Since, each character of string prefix needs to be printed, thus executing line 7 will take O(n) time. Line 10 and line 11 will also take O(n)
+        time combined due to string concatenation, as sum of rem, prefix and str.charAt(i) will always be n. Each function call therefore corresponds to O(n) work.
+
+        4. What is the total runtime?
+        Calling perm O(n * n!) times (as an upper bound) and each call takes O(n) time, the total runtime will not exceed O(n^2 * n!).
+     */
     public List<List<Integer>> permute(int[] nums) {
         List<List<Integer>> res = new ArrayList<>();
         if (nums == null) return res;
@@ -12,7 +31,7 @@ public class Permutation {
         return res;
     }
 
-    private void dfs(int[] nums, List<List<Integer>> res, List<Integer> list, boolean[] visited) {
+    private void dfs(int[] nums, List<List<Integer>> res, List<Integer> list, boolean[] used) {
         // base case
         if (list.size() == nums.length) {
             res.add(new ArrayList<>(list));
@@ -21,13 +40,13 @@ public class Permutation {
 
         // recursive case
         for (int i = 0; i < nums.length; ++i) {
-            if (visited[i]) continue;
+            if (used[i]) continue;
 
-            visited[i] = true;
+            used[i] = true;
             list.add(nums[i]);
-            dfs(nums, res, list, visited);
+            dfs(nums, res, list, used);
             list.remove(list.size() - 1);
-            visited[i] = false;
+            used[i] = false;
         }
     }
 
@@ -76,14 +95,14 @@ public class Permutation {
             if (i != 0 && nums[i] == nums[i - 1] && !visited[i - 1]) {
                 continue;
             }
+            if (visited[i]) continue;
 
-            if (!visited[i]) {
-                visited[i] = true;
-                list.add(nums[i]);
-                dfs2(res, list, nums, visited);
-                list.remove(list.size() - 1);
-                visited[i] = false;
-            }
+            visited[i] = true;
+            list.add(nums[i]);
+            dfs2(res, list, nums, visited);
+            list.remove(list.size() - 1);
+            visited[i] = false;
+
         }
     }
 
